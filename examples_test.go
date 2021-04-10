ea6d617858d5001accc81dc3208096fb9f0a9289
Andrea Parodi <andrea@parro.it>
2021-04-10 15:46:49 +0200

    pass

--- examples_test.go
+++ examples_test.go
@@ -3,14 +3,21 @@ package writefs_test
 import (
 	"embed"
 	"io/fs"
+
+	"github.com/parrogo/writefs"
 )
 
 //go:embed fixtures
 var fixtureRootFS embed.FS
 var fixtureFS, _ = fs.Sub(fixtureRootFS, "fixtures")
 
-// This example show how to use writefs.Func()
-func ExampleOpenFile() {
-	//fmt.Println(writefs.Func())
+// This example show how to use writefs.OpenFile()
+func ExampleOpenFile(fsys writefs.WriteFS) {
+	f, err := writefs.OpenFile(fsys, "filename", 0, fs.FileMode(0))
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	f.Write([]byte("ciao"))
 	// Output: 42
 }
