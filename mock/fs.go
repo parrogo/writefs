4a9723de89d90038ff65082aed126d2adaaa6089
Andrea Parodi <andrea@parro.it>
2021-04-11 13:24:51 +0200

    refactor

--- mock/fs.go
+++ mock/fs.go
@@ -38,6 +38,16 @@ var (
 	//_ writefs.MkDirFS  = &FS{}
 )
 
+// OpenFile implements writefs.WriteFS
+func (fsys *FS) OpenFile(name string, flag int, perm fs.FileMode) (writefs.FileWriter, error) {
+	args := fsys.Called(name, flag, perm)
+	res := args.Get(0)
+	if res == nil {
+		return nil, args.Error(1)
+	}
+	return res.(writefs.FileWriter), args.Error(1)
+}
+
 // MkDir implements writefs.MkDirFS
 func (fsys *FS) MkDir(name string, perm fs.FileMode) error {
 	args := fsys.Called(name, perm)
@@ -50,72 +60,66 @@ func (fsys *FS) Remove(name string) error {
 	return args.Error(0)
 }
 
-// OpenFile implements writefs.WriteFS
-func (fsys *FS) OpenFile(name string, flag int, perm fs.FileMode) (writefs.FileWriter, error) {
-	args := fsys.Called(name, flag, perm)
-	res := args.Get(0)
-	if res == nil {
-		return nil, args.Error(1)
-	}
-	return res.(writefs.FileWriter), args.Error(1)
+// calledWithPath abstracts away the code
+// shared by all other function called with a string
+// argument and that returns any,error
+func calledWithPath(fsys *FS, args mock.Arguments) (res interface{}, err error) {
+
+	res = args.Get(0)
+	return res, args.Error(1)
 }
 
 // Stat implements fs.StatFS
 func (fsys *FS) Stat(name string) (fs.FileInfo, error) {
-	args := fsys.Called(name)
-	res := args.Get(0)
+	res, err := calledWithPath(fsys, fsys.Called(name))
 	if res == nil {
-		return nil, args.Error(1)
+		return nil, err
 	}
-	return res.(fs.FileInfo), args.Error(1)
+	return res.(fs.FileInfo), err
+
 }
 
 // ReadFile implements fs.ReadFileFS
 func (fsys *FS) ReadFile(name string) ([]byte, error) {
-	args := fsys.Called(name)
-	res := args.Get(0)
+	res, err := calledWithPath(fsys, fsys.Called(name))
 	if res == nil {
-		return nil, args.Error(1)
+		return nil, err
 	}
-	return res.([]byte), args.Error(1)
+	return res.([]byte), err
 }
 
 // Sub implements fs.SubFS
 func (fsys *FS) Sub(dir string) (fs.FS, error) {
-	args := fsys.Called(dir)
-	res := args.Get(0)
+	res, err := calledWithPath(fsys, fsys.Called(dir))
 	if res == nil {
-		return nil, args.Error(1)
+		return nil, err
 	}
-	return res.(fs.FS), args.Error(1)
+	return res.(fs.FS), err
 }
 
 // Open implements fs.FS
 func (fsys *FS) Open(name string) (fs.File, error) {
-	args := fsys.Called(name)
-	res := args.Get(0)
+	res, err := calledWithPath(fsys, fsys.Called(name))
 	if res == nil {
-		return nil, args.Error(1)
+		return nil, err
 	}
-	return res.(fs.File), args.Error(1)
+	return res.(fs.File), err
 }
 
 // ReadDir implements fs.ReadDirFS
 func (fsys *FS) ReadDir(name string) ([]fs.DirEntry, error) {
-	args := fsys.Called(name)
-	res := args.Get(0)
+	res, err := calledWithPath(fsys, fsys.Called(name))
 	if res == nil {
-		return nil, args.Error(1)
+		return nil, err
 	}
-	return res.([]fs.DirEntry), args.Error(1)
+	return res.([]fs.DirEntry), err
 }
 
 // Glob implements fs.GlobFS
 func (fsys *FS) Glob(pattern string) ([]string, error) {
-	args := fsys.Called(pattern)
-	res := args.Get(0)
+	res, err := calledWithPath(fsys, fsys.Called(pattern))
 	if res == nil {
-		return nil, args.Error(1)
+		return nil, err
 	}
-	return res.([]string), args.Error(1)
+	return res.([]string), err
 }
